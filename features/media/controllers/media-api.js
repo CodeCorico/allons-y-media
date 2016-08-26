39909baa32068b43cc8f4eeb08a7ccd226da1093
Xavier Boubert <xavier@boubert.fr>
2016-08-26 10:08:10 +0200

    refactor(media api): remove useless isMember attribute

--- features/media/controllers/media-api.js
+++ features/media/controllers/media-api.js
@@ -6,7 +6,6 @@ require(path.resolve(__dirname, '../models/media-service-back.js'))();
 module.exports = [{
   method: 'POST',
   url: 'media',
-  isMember: true,
   controller: function($req, $res, $MediaService) {
     $MediaService.storeUploadedFile($req, function(fileData) {
       $res.send(fileData);
